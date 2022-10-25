a3a15f9226c2129b8d5480649ebdb1357b5767f0
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-10-25 20:09:50 +0100

    CFYP-31 - Added model cs files

---
+++ COMP3000-Project-Backend-API/Models/AirQualityInfo.cs
@@ -0,0 +1,10 @@
+namespace COMP3000_Project_Backend_API;
+
+public class AirQuality
+{
+    public int value;
+    public string? unit;
+    public DateTime timestamp;
+    public string? licenseInfo;
+    public Station? station;
+}
---
+++ COMP3000-Project-Backend-API/Models/LatLong.cs
@@ -0,0 +1,7 @@
+namespace COMP3000_Project_Backend_API;
+
+public class LatLong
+{
+    public float lat;
+    public float lng;
+}
