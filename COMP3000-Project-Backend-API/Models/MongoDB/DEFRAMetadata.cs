dd96e382bb7086df3c84e0865517152571466fd8
Lewis Trowbridge <lewbob9927@gmail.com>
2022-10-29 20:51:33 +0100

    CFYP-43 - Fixed mapping of data to DEFRAMetadata

--- COMP3000-Project-Backend-API/Models/MongoDB/DEFRAMetadata.cs
+++ COMP3000-Project-Backend-API/Models/MongoDB/DEFRAMetadata.cs
@@ -6,8 +6,12 @@ namespace COMP3000_Project_Backend_API.Models.MongoDB;
 public class DEFRAMetadata {
     [BsonId]
     public string? Id { get; set; }
+    [BsonElement("site_name")]
     public string SiteName { get; set; } = null!;
+    [BsonElement("start_date")]
     public DateTime StartDate { get; set; }
+    [BsonElement("end_date")]
     public DateTime EndDate { get; set; }
-    public float[] Coords { get; set; } = null!;
+    [BsonElement("coords")]
+    public double[] Coords { get; set; } = null!;
 }
