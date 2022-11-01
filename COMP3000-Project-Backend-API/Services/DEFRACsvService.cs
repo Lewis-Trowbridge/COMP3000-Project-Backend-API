1fb83216ac0d8cbcd98e73cc521533b65584def6
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-11-01 14:42:53 +0000

    CFYP-30 - Removed intro information from CSV string

--- COMP3000-Project-Backend-API/Services/DEFRACsvService.cs
+++ COMP3000-Project-Backend-API/Services/DEFRACsvService.cs
@@ -1,6 +1,7 @@
 using COMP3000_Project_Backend_API.Models;
 using COMP3000_Project_Backend_API.Models.MongoDB;
 using CsvHelper;
+using CsvHelper.Configuration;
 using System.Globalization;
 
 namespace COMP3000_Project_Backend_API.Services
@@ -24,14 +25,21 @@ namespace COMP3000_Project_Backend_API.Services
             }
             
             var contentString = await request.Content.ReadAsStringAsync();
+            contentString = RemoveHeaderLines(contentString);
+
             using (var contentStringReader = new StringReader(contentString))
             using (var csv = new CsvReader(contentStringReader, CultureInfo.InvariantCulture))
             {
-                
+                var records = csv.GetRecords<dynamic>();
             }
 
             return new AirQualityInfo();
 
+        }
+
+        private static string RemoveHeaderLines(string csvString)
+        {
+            return csvString.Split("ug/m-3\n")[1];
         }
     }
 }
