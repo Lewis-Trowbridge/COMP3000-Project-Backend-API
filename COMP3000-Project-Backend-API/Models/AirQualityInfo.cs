efbcb1042ead2664fd9e322152c31eb05f6ca27a
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-11-01 10:36:19 +0000

    CFYP-31 - Changed value from int to float

--- COMP3000-Project-Backend-API/Models/AirQualityInfo.cs
+++ COMP3000-Project-Backend-API/Models/AirQualityInfo.cs
@@ -2,7 +2,7 @@ namespace COMP3000_Project_Backend_API;
 
 public class AirQuality
 {
-    public int value;
+    public float value;
     public string? unit;
     public DateTime timestamp;
     public string? licenseInfo;
