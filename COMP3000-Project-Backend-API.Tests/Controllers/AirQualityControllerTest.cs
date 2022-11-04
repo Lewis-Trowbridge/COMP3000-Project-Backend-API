279e2dfaa4675977bdaf939d219a833bcd8ad072
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-11-04 10:21:52 +0000

    CFYP-30 - Added test for retrieval of data

--- COMP3000-Project-Backend-API.Tests/Controllers/AirQualityControllerTest.cs
+++ COMP3000-Project-Backend-API.Tests/Controllers/AirQualityControllerTest.cs
@@ -46,5 +46,50 @@ namespace COMP3000_Project_Backend_API.Tests.Controllers
             mockAirQualityService.Verify(x => x.GetAirQualityInfo(metadata2, testDatetime), Times.Once());
             mockAirQualityService.Verify(x => x.GetAirQualityInfo(metadata3, testDatetime), Times.Once());
         }
+
+        [Fact]
+        public async void AirQualityController_GetAirQuality_ReturnsDataInCorrectFormat()
+        {
+            var mockMetadataService = new Mock<IMetadataService>();
+            var mockAirQualityService = new Mock<IAirQualityService>();
+            var testBbox = new BoundingBox(0, 0, 0, 0);
+
+            var metadata1 = new DEFRAMetadata() { SiteName = "1" };
+            var metadata2 = new DEFRAMetadata() { SiteName = "2" };
+            var metadata3 = new DEFRAMetadata() { SiteName = "3" };
+
+            var metadataList = new List<DEFRAMetadata>()
+            {
+                metadata1,
+                metadata2,
+                metadata3
+            };
+
+            var testDatetime = DateTime.MinValue;
+
+            var testAirQualityRequest = new AirQualityRequest()
+            {
+                Bbox = testBbox,
+                Timestamp = testDatetime
+            };
+
+            mockMetadataService.Setup(x => x.GetAsync(testBbox)).ReturnsAsync(metadataList);
+
+            var airQuality1 = new AirQualityInfo() { Station = new Station() { Name = "1" } };
+            var airQuality2 = new AirQualityInfo() { Station = new Station() { Name = "2" } };
+            var airQuality3 = new AirQualityInfo() { Station = new Station() { Name = "3" } };
+            var expected = new AirQualityInfo[] { airQuality1, airQuality2, airQuality3 };
+            
+            mockAirQualityService.Setup(x => x.GetAirQualityInfo(metadata1, It.IsAny<DateTime>())).ReturnsAsync(airQuality1);
+            mockAirQualityService.Setup(x => x.GetAirQualityInfo(metadata2, It.IsAny<DateTime>())).ReturnsAsync(airQuality2);
+            mockAirQualityService.Setup(x => x.GetAirQualityInfo(metadata3, It.IsAny<DateTime>())).ReturnsAsync(airQuality3);
+
+
+            var controller = new AirQualityController(mockMetadataService.Object, mockAirQualityService.Object);
+
+            var actual = await controller.GetAirQuality(testAirQualityRequest);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
     }
 }
