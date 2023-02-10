ed9ba1eea3e6d6fbe5de18548ab53242d78b6de3
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-02-10 16:16:45 +0000

    CFYP-47 - Converted request to use GET format

--- COMP3000-Project-Backend-API/Models/BoundingBox.cs
+++ COMP3000-Project-Backend-API/Models/BoundingBox.cs
@@ -19,12 +19,16 @@ namespace COMP3000_Project_Backend_API.Models
         }
 
         [Required]
+        [FromQuery(Name = "bottomLeftX")]
         public double BottomLeftX { get; set; }
         [Required]
+        [FromQuery(Name = "bottomLeftY")]
         public double BottomLeftY { get; set; }
         [Required]
+        [FromQuery(Name = "topRightX")]
         public double TopRightX { get; set; }
         [Required]
+        [FromQuery(Name = "topRightY")]
         public double TopRightY { get; set; }
     }
 }
